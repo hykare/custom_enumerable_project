2c73c2fc813a36dfca1f3ec3940dd596206a4d2a
hykare <78385964+hykare@users.noreply.github.com>
2022-07-13 13:11:52 +0200

    implement 'each' for Array

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,12 @@
 module Enumerable
-  # Your code goes here
+
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    for i in 0..length - 1
+      yield self[i]
+    end
+    self
+  end
 end
